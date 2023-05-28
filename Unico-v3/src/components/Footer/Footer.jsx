286a686b046dd09791dfae16309eb1fb08e1768b
Shivansh Mehta <shivanshmehta0000@gmail.com>
2023-05-28 11:42:23 +0530

    cart-scroll + footer

--- Unico-v3/src/components/Footer/Footer.jsx
+++ Unico-v3/src/components/Footer/Footer.jsx
@@ -49,7 +49,7 @@ const Footer = () => {
         <div className="bottom-bar">
             <div className="bottom-bar-content">
                 <div className="fottext">
-                    ©Unico Foods, All Rights Reserved
+                <Link to="https://www.linkedin.com/company/arroz-solution/" className="link">Developed By Arroz Solutions</Link>
                 </div>
                 <img src={Payment} alt="" />
             </div>
